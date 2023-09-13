59769dc51602150e6f8c8f2d696d174d51f086a6
Monica Ramesh <monica98r@gmail.com>
2023-09-13 14:09:32 -0400

    functions lab complete

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
 // Code your solution in this file!
+
+function distanceFromHqInBlocks(pickupLocation) {
+  
+  // return Math.abs(pickupLocation-42); 
+
+  if(pickupLocation > 42) {
+    // codeBlock
+   return pickupLocation - 42 ;
+  }
+  if (pickupLocation < 42) {
+    return  42 - pickupLocation ;
+  }
+}
+
+console.log (distanceFromHqInBlocks (55) )
+
+function distanceFromHqInFeet(pickupLocation) {
+  return distanceFromHqInBlocks(pickupLocation) *264 
+}
+    
+  function distanceTravelledInFeet(start, destination) {
+    return Math.abs(destination-start)*264 
+  }
+
+  function calculatesFarePrice(start, destination) {
+    const distance = distanceTravelledInFeet(start, destination);
+    if (distance <=400){
+      return 0 ;
+    }
+    else if (distance >400 && distance <=2000){
+      return (distance-400) * 0.02;
+    }
+    else if (distance >2000 && distance <2500) {
+      return 25;
+    }
+    else {
+      return 'cannot travel that far' ;
+    }
+  }
